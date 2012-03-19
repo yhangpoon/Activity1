3e28fe5d5cd869f6d28ccd7257ddce0b0df16fc7
Greg Chambers <grg.sox@gmail.com>
2012-03-19 15:19:48 -0400

    Added ThreadLoan nested class with stubs.

--- src/Banker.java
+++ src/Banker.java
@@ -4,12 +4,93 @@
  * @author Yin Poon
  */
 
+import java.util.HashMap;
+
 /**
  * A passive, non-threaded class that other threads can use to request
  * resources. Resources will only be granted if doing so will not result in an
  * unsafe state.
  */
 public class Banker {
+	
+	private final int totalUnits; // Total number of resources
+	private int availableUnits; // Number of resources Banker has
+	private HashMap<Thread, ThreadLoan> threadClaims; // Registered threads with 
+												   // their max claims.
+	
+	/**
+	 * A class containing two integers representing a thread's current
+	 * resource allocation amount and the amount left before reaching
+	 * their max claim.
+	 */
+	private class ThreadLoan {
+		
+		private final int maxClaim;
+		private int allocated;
+		
+		/**
+		 * Creates a new ThreadLoan object with the given int parameter used
+		 * for determining the maximum claim that can be made by the thread.
+		 * It initially has no resources allocated to it.
+		 * 
+		 * @param int maxClaim
+		 */
+		public ThreadLoan(int maxClaim){
+			this.maxClaim = maxClaim;
+			this.allocated = 0;
+			
+		} // end constructor ThreadLoan
+		
+		/**
+		 * Returns the value of allocated.
+		 * 
+		 * @return int
+		 */
+		public synchronized int getAllocated(){
+			return this.allocated;
+			
+		} // end method getAllocated
+		
+		/**
+		 * Returns the difference between the amount currently allocated and
+		 * the maximum claim.
+		 * 
+		 * @return int
+		 */
+		public synchronized int getRemaining(){
+			return this.maxClaim - this.allocated;
+			
+		} // end method getRemaining
+		
+		/**
+		 * Sets the new amount of allocated resouces. Returns false if the
+		 * new amount would be illegal. Otherwise returns true.
+		 * 
+		 * @param int newAllocated
+		 * @return boolean
+		 */
+		public synchronized boolean setAllocated(int newAllocated){
+			//TODO
+			return true;
+			
+		} // end method setAllocated
+		
+		/**
+		 * Similar to the setAllocated() method but is used for making relative
+		 * changes instead of absolute changes. For example, decrementing the
+		 * amount by one can be done simply by calling this method and passing
+		 * it -1 as a value.
+		 * 
+		 * @param int delta
+		 * @return boolean
+		 */
+		public synchronized boolean deltaAllocated(int delta){
+			//TODO
+			return true;
+			
+		} // end method deltaAllocated
+		
+	} // end class ThreadLoan
 	
 	/**
 	 * Makes a new Banker object with nUnits number of reosurces and with no
@@ -18,7 +99,9 @@ public class Banker {
 	 * @param int nUnits
 	 */
 	public Banker(int nUnits){
-		//TODO
+		totalUnits = nUnits;
+		availableUnits = totalUnits;
+		threadClaims = new HashMap<Thread, ThreadLoan>();
 		
 	} // end constructor Banker
 	
