1f86dca393445d38dba868e3de7bc5cf7673e59e
Greg Chambers <grg.sox@gmail.com>
2012-03-19 14:36:16 -0400

    Added javadocs to Client and Driver classes.

--- src/Client.java
+++ src/Client.java
@@ -5,12 +5,12 @@
  */
 
 /**
- * 
+ * A threadable class that uses resources allocated to it by the Banker class.
  */
 public class Client extends Thread {
 
 	/**
-	 * 
+	 * Creates a new Client object, ready to run as a seperate thread.
 	 */
 	public Client(String name, Banker banker, int nUnits, int nRequests, 
 			long minSleepMillis, long maxSleepMillis){
@@ -19,7 +19,7 @@ public class Client extends Thread {
 	} // end constructor Client
 	
 	/**
-	 * 
+	 * Begins execution of the thread.
 	 */
 	public void run(){
 		//TODO
--- src/Driver.java
+++ src/Driver.java
@@ -5,12 +5,15 @@
  */
 
 /**
- * 
+ * Class containing the main method that sets up the program's initial state
+ * and then tells it to run.
  */
 public class Driver {
 
 	/**
-	 * @param args
+	 * The main method. Command line arguments are left unused.
+	 * 
+	 * @param String args
 	 */
 	public static void main(String[] args) {
 		// TODO
