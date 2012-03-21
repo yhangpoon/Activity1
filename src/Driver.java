93bb4f96ebcf4286ceb0fddb583a9d3a14749455
Jake Juby <jakejuby@gmail.com>
2012-03-20 20:50:35 -0400

    finishing up the driver class or so it seems

--- src/Driver.java
+++ src/Driver.java
@@ -4,11 +4,25 @@
  * @author Yin Poon
  */
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
  * Class containing the main method that sets up the program's initial state
  * and then tells it to run.
  */
 public class Driver {
+
+  //total number of clients in the system
+  final private static int totalClients;
+  //the number of requests each client will run
+  final private static int numOfRequests;
+  //the number of units the banker has
+  final private static int numOfUnits;
+  //minimum client sleep time
+  final private static long minSleepMillis;
+  //maximum client sleep time
+  final private static long maxSleepMillis;
 
 	/**
 	 * The main method. Command line arguments are left unused.
@@ -16,7 +30,25 @@ public class Driver {
 	 * @param String args
 	 */
 	public static void main(String[] args) {
-		// TODO
+    //banker object
+    Banker banker = new Banker(numOfUnits);
+
+    //list of clients to join on
+    List<Client> clients = new ArrayList<Client>();
+
+    //client initialization loop
+    for(int i = 0; i < totalClients; i++) {
+      Client t = new Client("CLIENT "+i, banker, numOfUnits, numOfRequests, minSleepMillis, maxSleepMillis);
+      //start the thread 
+      t.start();
+      clients.add(t);
+    }
+
+    for(Client c : clients) {
+      try {
+        c.join();
+      } catch (InterruptedException ignore) {}
+    }
 
 	} // end method main
 
